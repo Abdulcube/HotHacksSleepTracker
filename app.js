48d7ea70a1ae65d2098e533e961955596460756e
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2020-09-13 07:20:57 -0400

    js update

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function addEntry() {
-	//let entry = document.getElementById('add');
+	const container = document.getElementById('journal-container');
+	const temp = document.getElementsByTagName('template')[0];
+	const clone = temp.content.cloneNode(true);
+	container.prepend(clone);
+}
+
+function saveEntry() {
+	alert('Save successful!');
+}
+
+function deleteEntry() {
+	alert('You successfully deleted an entry');
 }
