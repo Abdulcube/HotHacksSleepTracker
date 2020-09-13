0bde2366aa6b7c643b3deb3b8cedcc7a3ec6167a
jpersh <66654320+jpersh@users.noreply.github.com>
2020-09-13 08:40:19 -0400

    Update pyflask.py

--- pyflask.py
+++ pyflask.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, send_file, Markup
 from forms import RegistrationForm, LoginForm
-from datetime import date
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+
+
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY']= '3a22684e1daddc472ed4211d667d0fb6'
@@ -15,7 +20,7 @@ def home():
 def sleep():
     return render_template('sleepdata.html', title = 'Sleep Data')
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register")
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -24,7 +29,7 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login")
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -36,6 +41,9 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+#displays graphs, from xlsx files
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
+	
